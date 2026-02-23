0699c69e55260f8f44ea852057c0b35c4c0bae2c
Muvafakpk <muwafakmoh@gmail.com>
2026-02-23 19:08:22 +0530

    image

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import Admin from "./app/pages/Admin";
 
 export default function App() {
   return <RouterProvider router={router} />;
